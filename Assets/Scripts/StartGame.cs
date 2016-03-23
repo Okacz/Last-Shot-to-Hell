882d3129def1a701ab803c2d7876b115e79aebbf
thympy <s12025@pjwstk.edu.pl>
2016-03-23 01:06:23 +0100

    menu

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartGame : MonoBehaviour {
+
+    public void StartTheGame()
+    {
+        Time.timeScale = 1;
+        Application.LoadLevel("scena1");
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+}
