3660a4f60d68afe2042fe0095df7f050eef69a6c
Okacz <michalwronski1@gmail.com>
2016-05-29 22:32:55 +0200

    healthbar beta

--- Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/GUIScript.cs
+++ Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/GUIScript.cs
@@ -2,26 +2,21 @@
 using System.Collections;
 
 public class GUIScript : MonoBehaviour {
-    float health;
+    
     public Texture t1;
+    public Texture t1_1;
     public Texture t2;
 	// Use this for initialization
 	void Start () {
-        health = GetComponent<ThirdPersonCharacter>().health;
 	}
     void OnGUI()
     {
-        GUI.VerticalScrollbar(new Rect(0, Screen.height - 100, 20, 150), 0, GetComponent<ThirdPersonCharacter>().health, 100, 0);
-        GUI.skin.verticalScrollbar.fixedWidth = 20;
-        /*GUI.DrawTexture(new Rect(200, Screen.height - 100, 100, 100), t1);
-        GUI.DrawTexture(new Rect(200, (((Screen.height - 100) * 2 + t1.height) / 2) - (t1.height*GetComponent<ThirdPersonCharacter>().health/100)/2, 100, GetComponent<ThirdPersonCharacter>().health), t2, ScaleMode.ScaleAndCrop);
+        
 
-        GUI.DrawTexture(new Rect(300, Screen.height - 100, 100, 100), t1);
-        GUI.DrawTexture(new Rect(300, Screen.height - 100, 100, GetComponent<ThirdPersonCharacter>().health), t2, ScaleMode.ScaleToFit);
-
-        GUI.DrawTexture(new Rect(400, Screen.height - 100, 100, 100), t1);
-        GUI.DrawTexture(new Rect(400, Screen.height - 100, 100, GetComponent<ThirdPersonCharacter>().health), t2, ScaleMode.StretchToFill);
-        */
+        GUI.DrawTexture(new Rect(0, Screen.height * 0.75f, 300, 150), t1_1, ScaleMode.ScaleToFit);
+        GUI.BeginGroup(new Rect(0, Screen.height * 0.75f + GetComponent<ThirdPersonCharacter>().health * 1.5f*(-1)+150, 300, 150));
+        GUI.DrawTexture(new Rect(0, GetComponent<ThirdPersonCharacter>().health * 1.5f-150, 300, 150), t2, ScaleMode.ScaleToFit);
+        GUI.EndGroup();
     }
 	// Update is called once per frame
 	void Update () {
